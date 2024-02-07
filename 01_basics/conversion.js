7168e21f9eda4fa9cb641d7095209d66b070c529
Subhajit Patra <patrasubhajit398@gmail.com>
2024-02-07 17:13:06 +0000

    conversion

---
+++ 01_basics/conversion.js
@@ -0,0 +1,34 @@
+//let score ="Subhajit"
+// console.log(typeof score)
+// console.log(typeof(score));
+
+//let valueInNumber =Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber)
+
+//"33"=> 33
+//"Subhajit"=>NaN
+//true=>1; false=>0
+
+// let isLoggedIn=1
+// let BooleanIsLoggedIn=Boolean (isLoggedIn)
+// console.log(BooleanIsLoggedIn);
+
+// let someNumber=33;
+// let stringNumber=String(someNumber)
+// console.log(typeof(stringNumber));
+
+// ############################ Operations  ##############################
+
+let value=3
+let negValue=-value
+//console.log(negValue)
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+
